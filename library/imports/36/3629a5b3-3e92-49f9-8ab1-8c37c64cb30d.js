d1a80df02151d3e5abc7753ca052e96bd345eb4d
zhangwanbei <zhang.wanbei@pufubao.net>
2018-05-16 17:58:53 +0800

    player action

--- library/imports/36/3629a5b3-3e92-49f9-8ab1-8c37c64cb30d.js
+++ library/imports/36/3629a5b3-3e92-49f9-8ab1-8c37c64cb30d.js
@@ -33,16 +33,91 @@ cc.Class({
         //         this._bar = value;
         //     }
         // },
+        // 主角跳跃高度
+        jumpHeight: 0,
+        // 主角跳跃持续时间
+        jumpDuration: 0,
+        // 最大移动速度
+        maxMoveSpeed: 0,
+        // 加速度
+        accel: 0
     },
 
     // LIFE-CYCLE CALLBACKS:
 
     // onLoad () {},
 
-    start: function start() {}
-}
+    start: function start() {},
 
-// update (dt) {},
-);
+
+    setJumpAction: function setJumpAction() {
+        // 跳跃上升
+        var jumpUp = cc.moveBy(this.jumpDuration, cc.p(0, this.jumpHeight)).easing(cc.easeCubicActionOut());
+        // 下落
+        var jumpDown = cc.moveBy(this.jumpDuration, cc.p(0, -this.jumpHeight)).easing(cc.easeCubicActionIn());
+        // 不断重复
+        return cc.repeatForever(cc.sequence(jumpUp, jumpDown));
+    },
+
+    setInputControl: function setInputControl() {
+        var self = this;
+        // 添加键盘事件监听
+        // 有按键按下时，判断是否是我们指定的方向控制键，并设置向对应方向加速
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, function (event) {
+            switch (event.keyCode) {
+                case cc.KEY.a:
+                    self.accLeft = true;
+                    break;
+                case cc.KEY.d:
+                    self.accRight = true;
+                    break;
+            }
+        });
+
+        // 松开按键时，停止向该方向的加速
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, function (event) {
+            switch (event.keyCode) {
+                case cc.KEY.a:
+                    self.accLeft = false;
+                    break;
+                case cc.KEY.d:
+                    self.accRight = false;
+                    break;
+            }
+        });
+    },
+
+    onLoad: function onLoad() {
+        // 初始化跳跃动作
+        this.jumpAction = this.setJumpAction();
+        this.node.runAction(this.jumpAction);
+
+        // 加速度方向开关
+        this.accLeft = false;
+        this.accRight = false;
+        // 主角当前水平方向速度
+        this.xSpeed = 0;
+
+        // 初始化键盘输入监听
+        this.setInputControl();
+    },
+
+    update: function update(dt) {
+        // 根据当前加速度方向每帧更新速度
+        if (this.accLeft) {
+            this.xSpeed -= this.accel * dt;
+        } else if (this.accRight) {
+            this.xSpeed += this.accel * dt;
+        }
+        // 限制主角的速度不能超过最大值
+        if (Math.abs(this.xSpeed) > this.maxMoveSpeed) {
+            // if speed reach limit, use max speed with current direction
+            this.xSpeed = this.maxMoveSpeed * this.xSpeed / Math.abs(this.xSpeed);
+        }
+
+        // 根据当前速度更新主角的位置
+        this.node.x += this.xSpeed * dt;
+    }
+});
 
 cc._RF.pop();
